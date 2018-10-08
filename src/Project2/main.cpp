08316d91efc2845f5492782e9e1b6ddb9914c049
Pol Jaumira <pjaumira@gmail.com>
2018-10-09 00:14:10 +0200

    Scenes montades

--- src/Project2/main.cpp
+++ src/Project2/main.cpp
@@ -47,18 +47,24 @@ int main(int, char*[])
 
 	// --- SPRITES ---
 
-		//Background
+		//Background Scene 1
 		SDL_Texture* bgTexture{ IMG_LoadTexture(m_renderer, "../../res/img/bg.jpg")};
 		if (bgTexture == nullptr) throw "Error: bgTexture init";
 		SDL_Rect bgRect{ 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
 
+		//Background Scene 2
+		SDL_Texture* bgTextureC{ IMG_LoadTexture(m_renderer, "../../res/img/bgCastle.jpg") };
+		if (bgTexture == nullptr) throw "Error: bgTexture init";
+
 		//Player Cursor
 		SDL_Texture *mouseTexture{ IMG_LoadTexture(m_renderer,"../../res/img/kintoun.png") };
-		if (mouseTexture == nullptr) throw "Error: PlayerTexture init";
+		if (mouseTexture == nullptr) throw "Error: MouseTexture init";
 		SDL_Rect mouseRect{ 0,0, 90, 60 };
 		SDL_Rect mouseTarget{ 0,0,80,80 };
 
 	//-->Animated Sprite ---
+
+		//Player1
 		SDL_Texture *playerTexture{ IMG_LoadTexture(m_renderer, "../../res/img/sdp01.png") };
 		SDL_Rect playerRect, playerPosition;
 		int textwidth, textheight, framewidth, frameheight;
@@ -71,6 +77,8 @@ int main(int, char*[])
 		playerPosition.w = playerRect.w = frameheight;
 		int frametime = 0;
 
+		//Player2
+
 	// --- TEXT ---
 		TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf",80) };
 		if (font == nullptr) throw "no trobo la font";
@@ -80,21 +88,62 @@ int main(int, char*[])
 		if (tmpSurfP == nullptr) throw "no puc fer surface P";
 		SDL_Texture *textTextureP{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfP) };
 		SDL_Rect textRectP{ 340,75, tmpSurfP->w, tmpSurfP->h };
-		SDL_FreeSurface(tmpSurfP);
 
-		//Sound
-		SDL_Surface *tmpSurfS{ TTF_RenderText_Blended(font, "Sound ON", SDL_Color{116,25,20,255}) };
+		// Play Hover
+		SDL_Surface *tmpSurfPH{ TTF_RenderText_Blended(font, "Play", SDL_Color{ 150,190,20,255 }) };
+		if (tmpSurfPH == nullptr) throw "no puc fer surface PH";
+		SDL_Texture *textTexturePH{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfPH) };
+
+		//Play not Hover
+		SDL_Surface *tmpSurfPNH{ TTF_RenderText_Blended(font, "Play", SDL_Color{ 50,100,200,255 }) };
+		if (tmpSurfPNH == nullptr) throw "no puc fer surface PNH";
+		SDL_Texture *textTexturePNH{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfPNH) };
+
+		//Sound ON
+		SDL_Surface *tmpSurfS{ TTF_RenderText_Blended(font, "Sound ON", SDL_Color{16,255,20,255}) };
 		if (tmpSurfS == nullptr) throw "no puc fer surface S";
 		SDL_Texture *textTextureS{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfS) };
 		SDL_Rect textRectS{ 280,200, tmpSurfS->w, tmpSurfS->h };
-		SDL_FreeSurface(tmpSurfS);
+
+		// Sound OFF
+		SDL_Surface *tmpSurfSF{ TTF_RenderText_Blended(font, "Sound OFF", SDL_Color{ 255,25,20,255 }) };
+		if (tmpSurfSF == nullptr) throw "no puc fer surface SF";
+		SDL_Texture *textTextureSF{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfSF) };
+
+		//Sound Normal
+		SDL_Surface *tmpSurfSN{ TTF_RenderText_Blended(font, "Sound ON", SDL_Color{ 16,255,20,255 }) };
+		if (tmpSurfSN == nullptr) throw "no puc fer surface SN";
+		SDL_Texture *textTextureSN{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfSN) };
 
 		//Quit
-		SDL_Surface *tmpSurfQ{ TTF_RenderText_Blended(font, "Quit", SDL_Color{ 200,100,100,255 }) };
+		SDL_Surface *tmpSurfQ{ TTF_RenderText_Blended(font, "Quit", SDL_Color{ 230,100,100,255 }) };
 		if (tmpSurfQ == nullptr) throw "no puc fer surface Q";
 		SDL_Texture *textTextureQ{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfQ) };
 		SDL_Rect textRectQ{ 330,325, tmpSurfQ->w, tmpSurfQ->h };
-		SDL_FreeSurface(tmpSurfQ);
+
+		// Quit Hover
+		SDL_Surface *tmpSurfQH{ TTF_RenderText_Blended(font, "Quit", SDL_Color{ 255,255,255,255 }) };
+		if (tmpSurfQH == nullptr) throw "no puc fer surface QH";
+		SDL_Texture *textTextureQH{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfQH) };
+
+		// Quit No Hover
+		SDL_Surface *tmpSurfQNH{ TTF_RenderText_Blended(font, "Quit", SDL_Color{ 230,100,100,255 }) };
+		if (tmpSurfQNH == nullptr) throw "no puc fer surface QNH";
+		SDL_Texture *textTextureQNH{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfQNH) };
+
+		//Comptador Jugador 1
+		SDL_Surface *tmpSurfC1{ TTF_RenderText_Blended(font, "Punts P1", SDL_Color{ 255,10,10,255 }) };
+		if (tmpSurfC1 == nullptr) throw "no puc fer surface C1";
+		SDL_Texture *textTextureC1{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfC1) };
+		SDL_Rect textRectC1{ 10,10, tmpSurfC1->w, tmpSurfC1->h };
+		SDL_FreeSurface(tmpSurfC1);
+
+		//Comptador Jugador 2
+		SDL_Surface *tmpSurfC2{ TTF_RenderText_Blended(font, "Punts P2", SDL_Color{ 255,10,10,255 }) };
+		if (tmpSurfC2 == nullptr) throw "no puc fer surface C2";
+		SDL_Texture *textTextureC2{ SDL_CreateTextureFromSurface(m_renderer, tmpSurfC2) };
+		SDL_Rect textRectC2{ 10,10, tmpSurfC2->w, tmpSurfC2->h };
+		SDL_FreeSurface(tmpSurfC2);
 
 		//Fin
 		TTF_CloseFont(font);
@@ -114,8 +163,28 @@ int main(int, char*[])
 	// --- GAME LOOP ---
 	SDL_Event event;
 	frameStart = SDL_GetTicks();
+
+	//boolea per les tecles i events
 	bool isRunning = true;
 	while (isRunning) {
+
+		struct vect2 {
+			int x;
+			int y;
+		};
+
+		int Scene = 1;
+		bool sound = true;
+
+		vect2 mouseCoord;
+		mouseCoord.x = 0;
+		mouseCoord.y = 0;
+
+		//init bollean array
+		bool key[10] = {};
+		for (int i = 0; i > 9;i++) {
+			key[i] = false;
+		};
 
 		// HANDLE EVENTS
 		while (SDL_PollEvent(&event)) { // per poder cargar totes els ordres rebudes del stack
@@ -123,20 +192,36 @@ int main(int, char*[])
 			case SDL_QUIT:		
 				isRunning = false; 
 				break;
-			case SDL_KEYDOWN:	
-				if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false;
+			case SDL_KEYDOWN:
+				//Esc tenca la pantalla
+				if (event.key.keysym.sym == SDLK_ESCAPE) key[0] = true;
+				//Controls Player 1
+				if (event.key.keysym.sym == SDLK_UP) key[1] = true;
+				if (event.key.keysym.sym == SDLK_DOWN) key[2] = true;
+				if (event.key.keysym.sym == SDLK_LEFT) key[3] = true;
+				if (event.key.keysym.sym == SDLK_RIGHT) key[4] = true;
+				// Controls Player 2
+				if (event.key.keysym.sym == SDLK_ESCAPE) key[5] = true;
+				if (event.key.keysym.sym == SDLK_ESCAPE) key[6] = true;
+				if (event.key.keysym.sym == SDLK_ESCAPE) key[7] = true;
+				if (event.key.keysym.sym == SDLK_ESCAPE) key[8] = true;
 				break;
 			case SDL_MOUSEMOTION:
 				mouseTarget.x = event.motion.x -30; 
 				mouseTarget.y = event.motion.y -30;
 				break;
 			case SDL_MOUSEBUTTONDOWN:
+				if (event.button.button == SDL_BUTTON_LEFT) key[9] = true;
+				SDL_GetMouseState(&mouseCoord.x, &mouseCoord.y);
 				break;
 			default:;
 			}
 		}
 
 		// UPDATE
+
+		if (key[0] == true) isRunning = false;
+
 		mouseRect.x += (mouseTarget.x - mouseRect.x) / 10;
 		mouseRect.y += (mouseTarget.y - mouseRect.y) / 10;
 		frametime++;
@@ -145,7 +230,7 @@ int main(int, char*[])
 			playerRect.x += framewidth;
 			if (playerRect.x >= textwidth)
 				playerRect.x = 0;
-		}
+		};
 		deltaTime = (clock() - lastTime);
 		lastTime = clock();
 		deltaTime /= CLOCKS_PER_SEC;
@@ -154,20 +239,72 @@ int main(int, char*[])
 		frameTime = SDL_GetTicks() - frameStart;
 		if (frameTime < DELAY_TIME) {
 			SDL_Delay((int)(DELAY_TIME - frameTime));
-		}
+		};
+
+		//Si es la escena del menu
+		if (Scene == 1) {
+			//hover events
+			if (mouseTarget.x >= textRectP.x && mouseTarget.x <= textRectP.x + textRectP.w && mouseTarget.y >= textRectP.y && mouseTarget.y <= textRectP.y + textRectP.h) {
+				textTextureP = textTexturePH;
+				SDL_FreeSurface(tmpSurfP);
+			}
+			else {
+				textTextureP = textTexturePNH;
+				SDL_FreeSurface(tmpSurfP);
+			}
+
+			if (mouseTarget.x >= textRectQ.x && mouseTarget.x <= textRectQ.x + textRectQ.w && mouseTarget.y >= textRectQ.y && mouseTarget.y <= textRectQ.y + textRectQ.h) {
+				textTextureQ = textTextureQH;
+				SDL_FreeSurface(tmpSurfQ);
+			}
+			else {
+				textTextureQ = textTextureQNH;
+				SDL_FreeSurface(tmpSurfQ);
+			};
+
+			//eventos de apretar
+			if (mouseCoord.x >= textRectS.x && mouseCoord.x <= textRectS.x + textRectS.w && mouseCoord.y >= textRectS.y && mouseCoord.y <= textRectS.y + textRectS.h) {
+				if (sound) {
+					textTextureS = textTextureSF;
+					SDL_FreeSurface(tmpSurfS);
+						Mix_HaltMusic();
+						sound = false;
+				}
+				if (sound = false) {
+					textTextureS = textTextureSN;
+					SDL_FreeSurface(tmpSurfS);
+					Mix_PlayMusic(ost, -1);
+					sound = true;
+				}
+			};
+
+			if (mouseCoord.x >= textRectQ.x && mouseCoord.x <= textRectQ.x + textRectQ.w && mouseCoord.y >= textRectQ.y && mouseCoord.y <= textRectQ.y + textRectQ.h) {
+				isRunning = false;
+			}
+		};
+
+		if (mouseCoord.x >= textRectP.x && mouseCoord.x <= textRectP.x + textRectP.w && mouseCoord.y >= textRectP.y && mouseCoord.y <= textRectP.y + textRectP.h) {
+			Scene = 2;
+		};
+
+		//Si es la escena de joc
+		if (Scene == 2) {
+			bgTexture = bgTextureC;
+
+		};
 
 		// DRAW
 		SDL_RenderClear(m_renderer);
 		//Background
 		SDL_RenderCopy(m_renderer, bgTexture, nullptr, &bgRect);
 		//Text
+		SDL_RenderCopy(m_renderer, playerTexture, &playerRect, &playerPosition);
 		SDL_RenderCopy(m_renderer, textTextureP, nullptr, &textRectP);
 		SDL_RenderCopy(m_renderer, textTextureS, nullptr, &textRectS);
 		SDL_RenderCopy(m_renderer, textTextureQ, nullptr, &textRectQ);
-		//Player
+		//Mouse Scene 1
 		SDL_RenderCopy(m_renderer,mouseTexture , nullptr, &mouseRect);
 		SDL_RenderPresent(m_renderer);
-		SDL_RenderCopy(m_renderer, playerTexture, &playerRect, &playerPosition);
 	}
 
 	// --- DESTROY ---
